f7ee3853a5bb9960fc4c8e1d4b0881daebcb4a76
Zerui Wei <wezerui@umich.edu>
2021-02-04 10:41:31 -0500

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@
 var data = require('../data.json');
 
 exports.view = function(request, response){
-	console.log(data);
-	response.render('index');
+	console.log(data)
+	response.render('index', data);
 };
